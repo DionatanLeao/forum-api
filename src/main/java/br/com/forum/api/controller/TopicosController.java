72b66114a6303d9daec20315b4065fff39c6a562
DionatanLeao <dionatan.ti@outlook.com>
2020-11-09 16:48:43 -0300

    Atualizando tópicos

--- src/main/java/br/com/forum/api/controller/TopicosController.java
+++ src/main/java/br/com/forum/api/controller/TopicosController.java
@@ -7,9 +7,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,8 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.forum.api.controller.dto.DetalhesTopicoDto;
 import br.com.forum.api.controller.dto.TopicoDto;
-import br.com.forum.api.controller.form.Topicoform;
+import br.com.forum.api.controller.form.TopicoForm;
+import br.com.forum.api.controller.form.UpdateTopicoForm;
 import br.com.forum.api.entity.Topico;
 import br.com.forum.api.repository.CursoRepository;
 import br.com.forum.api.repository.TopicoRepository;
@@ -32,6 +35,15 @@ public class TopicosController {
 	@Autowired
 	private CursoRepository cursoRepository;
 	
+	@PostMapping
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriBuilder) {
+		Topico topico = topicoForm.converter(cursoRepository);
+		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();		
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+	
 	@GetMapping
 	public List<TopicoDto> listar(String nomeCurso) {
 		if(nomeCurso == null) {
@@ -43,18 +55,16 @@ public class TopicosController {
 		}
 	}
 	
-	@PostMapping
-	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid Topicoform topicoForm, UriComponentsBuilder uriBuilder) {
-		Topico topico = topicoForm.converter(cursoRepository);
-		topicoRepository.save(topico);
-		
-		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();		
-		return ResponseEntity.created(uri).body(new TopicoDto(topico));
-	}
-	
 	@GetMapping("/{id}")
 	public DetalhesTopicoDto detalhar(@PathVariable Long id) {
 		Topico topico = topicoRepository.getOne(id);
 		return new DetalhesTopicoDto(topico);
+	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid UpdateTopicoForm form) {
+		Topico topico = form.update(id, topicoRepository);
+		return ResponseEntity.ok(new TopicoDto(topico));
 	}
 }
--- src/main/java/br/com/forum/api/controller/form/UpdateTopicoForm.java
+++ src/main/java/br/com/forum/api/controller/form/UpdateTopicoForm.java
@@ -1,54 +1,46 @@
 package br.com.forum.api.controller.form;
 
-
-
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
-import br.com.forum.api.entity.Curso;
 import br.com.forum.api.entity.Topico;
-import br.com.forum.api.repository.CursoRepository;
+import br.com.forum.api.repository.TopicoRepository;
 
-public class Topicoform {
-	
+public class UpdateTopicoForm {
+
 	@NotNull
 	@NotEmpty
 	@Length(min = 5)
 	private String titulo;
-	
+
 	@NotNull
 	@NotEmpty
 	@Length(min = 10)
 	private String mensagem;
-	
-	@NotNull
-	@NotEmpty
-	private String nomeCurso;
-	
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getMensagem() {
 		return mensagem;
 	}
+
 	public void setMensagem(String mensagem) {
 		this.mensagem = mensagem;
 	}
-	public String getNomeCurso() {
-		return nomeCurso;
+
+	public Topico update(Long id, TopicoRepository topicoRepository) {
+		Topico topico = topicoRepository.getOne(id);
+		topico.setTitulo(this.titulo);
+		topico.setMensagem(this.mensagem);
+		return topico;
 	}
-	public void setNomeCurso(String nomeCurso) {
-		this.nomeCurso = nomeCurso;
-	}
-	public Topico converter(CursoRepository cursoRepository) {
-		Curso curso = cursoRepository.findByNome(nomeCurso);
-		return new Topico(titulo, mensagem, curso);
-	}
-	
-	
+
 }
