8ca53c85681bcd66e39c197e56b57a7b848fa772
DionatanLeao <dionatan.ti@outlook.com>
2020-11-09 17:08:41 -0300

    Removendo tópicos

--- src/main/java/br/com/forum/api/controller/TopicosController.java
+++ src/main/java/br/com/forum/api/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +37,7 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriBuilder) {
 		Topico topico = topicoForm.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -66,5 +68,12 @@ public class TopicosController {
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid UpdateTopicoForm form) {
 		Topico topico = form.update(id, topicoRepository);
 		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		topicoRepository.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 }
