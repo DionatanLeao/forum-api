cafd767fde770fa44907c5cba30d17ede2fbc423
DionatanLeao <dionatan.ti@outlook.com>
2020-11-11 12:37:16 -0300

    Recuperando o token do header Authorization

--- src/main/java/br/com/forum/api/security/SecurityConfiguration.java
+++ src/main/java/br/com/forum/api/security/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -38,7 +39,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
 				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll().antMatchers(HttpMethod.POST, "/auth").permitAll()
 				.anyRequest().authenticated().and().csrf().disable().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().addFilterBefore(new TokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 	// Configurações de recursos estáticos(js, css, imagens, etc.)
--- src/main/java/br/com/forum/api/security/TokenFilter.java
+++ src/main/java/br/com/forum/api/security/TokenFilter.java
@@ -1,5 +1,31 @@
 package br.com.forum.api.security;
 
-public class TokenFilter {
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class TokenFilter extends OncePerRequestFilter{
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		String token = recuperarToken(request);
+		System.out.println(token);
+	}
+
+	private String recuperarToken(HttpServletRequest request) {
+		String token = request.getHeader("Authorization");
+		if(token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+			return null;
+		}
+		
+		return token.substring(7, token.length());
+	}
 
 }
